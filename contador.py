8812b84339d59b574b8da3de197d3df0008ef4ee
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2024-09-07 12:07:31 -0300

    commit 3 - Criada a funcao meia_hora

--- contador.py
+++ contador.py
@@ -36,53 +36,36 @@ def main(page: ft.Page):
     
     ################# 30 min #################
     def meia_hora(e):
-        block.visible = False
-        text_c.size = 60
-        for m in range(29, -1, -1):
-            text_c.value = f"{m}"
+        m = 29
+        while True:
+            for s in range(59, -1, -1):
+                text_c.value = f"{m}:{s}"
+                if s == 0:
+                    m = m -1
+                page.update()
+                sleep(1)
             if m == 0:
                 text_c.value = 'Fim!'
                 frase = random.choice(jdados['frase'])
                 h2.value= f'{frase}'
-            page.update()
-            sleep(1)
-        alarme(e)
+                page.update()
+                alarme(e)
+                break
+        block.visible = True
+        page.update()    
 
     ################# CONTADOR #################
     def contador(e):
-        if block.value == '':
-            text_c.value = 'Vazio!'
-            page.update()
-            sleep(3)
-            text_c.value = ''
-            page.update()
-        else:
-            block.visible = False
-            text_c.size = 60
-            page.update()
-            min = int(block.value)
-            while True:
-                for i in range(59, -1, -1):
-                    text_c.value = f"{min}:{i}"
-                    if i == 0:
-                        min = min -1
-                    page.update()
-                    sleep(1)
-                if min == 0:
-                    text_c.value = 'Fim!'
-                    frases = random.choice(jdados['frase'])
-                    h2.value = f'{frases}'
-                    alarme(e)    
-                    page.update()
-                    break
-            sleep(5)
-            block.visible = True
-            page.update()
+        block.visible = False
+        text_c.size = 60
+
+        if block.value == "30 min":
+            meia_hora(e)
         
     btn = ft.ElevatedButton(
         text= 'Start',
         bgcolor= '#f4f1ed',
-        on_click= meia_hora,
+        on_click= contador,
         color= '#10110d'
     )
 
